8c28af763de7acf30481b36447f0ed7bc92d8f08
Eduardo J. Guerra <edujguerra@gmail.com>
2023-11-24 20:06:13 -0300

    comit 2

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -2,9 +2,10 @@ package br.com.alura.mvc.mudi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MudiApplication {
+public class MudiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MudiApplication.class, args);
